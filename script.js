82620fdb41c9a512ce6919e15dbacfb58ee23bf3
Shiv061 <shivansh0200@gmail.com>
2019-04-23 21:45:44 +0530

    Deploying

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const profile = require('./controllers/profile');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-clean-75215',
     user : 'postgres',
     password : 'pesit123',
     database : 'smartbrain'
